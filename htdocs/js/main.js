1c19c57e6bb4b918d422e55ffb12f4dc6310bb5e
Bart Moorman <bmoorman@Xenopus.id10t.us>
2018-01-01 06:00:52 -0700

    Start adding error handling Retain chevron status on library refresh Move all DOM modifications to functions

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
   function addLibrary(library) {
     $(`<div id='library-${library.id}'></div>`)
       .appendTo('#libraries');
-    $(`<h4 id='library-${library.id}-summary'>${library.id} : ${library.name}<span class='fas fa-redo text-muted float-right refresh-library' onclick='void(0)' data-library='${JSON.stringify(library)}'></span></h4>`)
+    $(`<h4 id='library-${library.id}-summary'>${library.id} : ${library.name}<span class='fa fa-redo text-muted float-right refresh-library' onclick='void(0)' data-library='${JSON.stringify(library)}'></span></h4>`)
       .appendTo(`#library-${library.id}`);
     $(`<div id='library-${library.id}-progress' class='progress mb-3'></div>`)
       .appendTo(`#library-${library.id}`);
@@ -20,7 +20,7 @@ $(document).ready(function() {
   }
 
   function addLibraryDetail(library, status) {
-    $(`<span class='badge badge-pill badge-${statuses[status.status].class} ml-2 library-${library.id}-status'>${status.count.toLocaleString()}<span class='fa fa-chevron-down text-primary ml-1 toggle-sections' onclick='void(0)' data-library='${JSON.stringify(library)}' data-status='${JSON.stringify(status)}'></span></span>`)
+    $(`<span id='library-${library.id}-${status.status}-summary' class='badge badge-pill badge-${statuses[status.status].class} ml-2 library-${library.id}-status toggle-sections' style='cursor:context-menu' onclick='void(0)' data-library='${JSON.stringify(library)}' data-status='${JSON.stringify(status)}'>${status.count.toLocaleString()}<span class='fa fa-chevron-down ml-1 sections-collapse-icon'></span></span>`)
       .appendTo(`#library-${library.id}-summary`);
     $(`<div class='progress-bar progress-bar-striped bg-${statuses[status.status].class} library-${library.id}-status' style='width:${status.count*100/library.count}%'></div>`)
       .appendTo(`#library-${library.id}-progress`);
@@ -34,11 +34,11 @@ $(document).ready(function() {
   function addLibrarySection(library, status, section) {
     $(`<div id='library-${library.id}-${status.status}-section-${section.id}' class='library-status-section'></div>`)
       .appendTo(`#library-${library.id}-${status.status}-sections`);
-    $(`<div class='card-header'><span>${section.root_path}<span class='badge badge-pill badge-dark ml-2'>${section.count.toLocaleString()}<span class='fa fa-chevron-down text-muted ml-1 toggle-section-details' onclick='void(0)' data-library='${JSON.stringify(library)}' data-status='${JSON.stringify(status)}' data-section='${JSON.stringify(section)}'></span></span></span></div>`)
+    $(`<div class='card-header'><span>${section.root_path}<span id='library-${library.id}-${status.status}-section-${section.id}-summary' class='badge badge-pill badge-dark ml-2 toggle-section-details' style='cursor:context-menu' onclick='void(0)' data-library='${JSON.stringify(library)}' data-status='${JSON.stringify(status)}' data-section='${JSON.stringify(section)}'>${section.count.toLocaleString()}<span class='fa fa-chevron-down ml-1 section-details-collapse-icon'></span></span></span></div>`)
       .appendTo(`#library-${library.id}-${status.status}-section-${section.id}`);
   }
 
-  function addLibrarySectionStatus(library, status, section) {
+  function addLibrarySectionDetails(library, status, section) {
     $(`<div id='library-${library.id}-${status.status}-section-${section.id}-details' class='card-body' data-section='${JSON.stringify(section)}'></div>`)
       .appendTo(`#library-${library.id}-${status.status}-section-${section.id}`);
   }
@@ -53,6 +53,38 @@ $(document).ready(function() {
     }
   }
 
+  function syncStartStatus(selector) {
+    $(selector).removeClass('fa-exclamation-triangle').addClass('fa-sync fa-spin');
+  }
+
+  function syncStopStatus(selector) {
+    $(selector).on('animationiteration', function() {
+      $(this).removeClass('fa-spin');
+    });
+  }
+
+  function syncErrorStatus(selector) {
+    $(selector).on('animationiteration', function() {
+      $(this).removeClass('fa-sync fa-spin').addClass('fa-exclamation-triangle');
+    });
+  }
+
+  function sectionOpen(selector) {
+    $(selector).show();
+  }
+
+  function sectionClose(selector) {
+    $(selector).hide();
+  }
+
+  function sectionOpenStatus(selector) {
+    $(selector).removeClass('fa-chevron-down').addClass('fa-chevron-up');
+  }
+
+  function sectionCloseStatus(selector) {
+      $(selector).removeClass('fa-chevron-up').addClass('fa-chevron-down');
+  }
+
   $.get('query.php', {"function": "getStatuses"})
     .done(function(data) {
       statuses = $.parseJSON(data);
@@ -61,79 +93,81 @@ $(document).ready(function() {
       });
     });
 
-  $('#libraries-loading').addClass('fa-spin');
-  $.get('query.php', {"function": "getLibraries"})
-    .done(function(data) {
-      var libraries = $.parseJSON(data);
+  syncStartStatus('#libraries-loading');
+  $.getJSON('query.php', {"function": "getLibraries"})
+    .done(function(libraries) {
       $.each(libraries, function(libraryID, library) {
         addLibrary(library);
         $.each(library.details, function(libraryDetailID, libraryDetail) {
           addLibraryDetail(library, libraryDetail);
         });
       });
-      $('#libraries-loading').on('animationiteration', function() {
-        $(this).removeClass('fa-spin');
-      });
+      syncStopStatus('#libraries-loading');
+    })
+    .fail(function(jqxhr, textStatus, errorThrow) {
+      console.log(`getLibraries failed: ${jqxhr.status} (${jqxhr.statusText}), ${textStatus}, ${errorThrown}`);
+      syncErrorStatus('#libraries-loading');
     });
 
-  $(document).on('click', 'span.toggle-sections', function() {
+  $(document).on('click', '.toggle-sections', function() {
     var library = $(this).data('library');
     var status = $(this).data('status');
     if ($(`#library-${library.id}-${status.status}-sections`).is(':hidden')) {
-      $(`#library-${library.id}-${status.status}-sections`).show();
+      sectionOpen(`#library-${library.id}-${status.status}-sections`);
+      sectionOpenStatus($(this).children('.sections-collapse-icon'));
       if (!$(`#library-${library.id}-${status.status}-sections`).children('.library-status-section').length) {
-        $('#libraries-loading').addClass('fa-spin');
-        $.get('query.php', {"function": "getLibrarySections", "library": library.id, "status": status.status})
-          .done(function(data) {
-            var librarySections = $.parseJSON(data);
+        syncStartStatus('#libraries-loading');
+        $.getJSON('query.php', {"function": "getLibrarySections", "library": library.id, "status": status.status})
+          .done(function(librarySections) {
             $.each(librarySections, function(librarySectionID, librarySection) {
               addLibrarySection(library, status, librarySection);
             });
-            $('#libraries-loading').on('animationiteration', function() {
-              $(this).removeClass('fa-spin');
-            });
+            syncStopStatus('#libraries-loading');
+          })
+          .fail(function(jqxhr, textStatus, errorThrown) {
+            console.log(`getLibrarySections failed: ${jqxhr.status} (${jqxhr.statusText}), ${textStatus}, ${errorThrown}`);
+            syncErrorStatus('#libraries-loading');
           });
       }
-      $(this).removeClass('fa-chevron-down').addClass('fa-chevron-up');
     } else if ($(`#library-${library.id}-${status.status}-sections`).is(':visible')) {
-      $(`#library-${library.id}-${status.status}-sections`).hide();
-      $(this).removeClass('fa-chevron-up').addClass('fa-chevron-down');
+      sectionClose(`#library-${library.id}-${status.status}-sections`);
+      sectionCloseStatus($(this).children('.sections-collapse-icon'));
     }
   });
 
-  $(document).on('click', 'span.toggle-section-details', function() {
+  $(document).on('click', '.toggle-section-details', function() {
     var library = $(this).data('library');
     var status = $(this).data('status');
     var section = $(this).data('section');
     if (!$(`#library-${library.id}-${status.status}-section-${section.id}-details`).length) {
-      $('#libraries-loading').addClass('fa-spin');
-      addLibrarySectionStatus(library, status, section);
-      $.get('query.php', {"function": "getLibrarySectionDetails", "library": library.id, "status": status.status, "section": section.id})
-        .done(function(data) {
-          var librarySectionDetails = $.parseJSON(data);
+      syncStartStatus('#libraries-loading');
+      addLibrarySectionDetails(library, status, section);
+      sectionOpenStatus($(this).children('.section-details-collapse-icon'));
+      $.getJSON('query.php', {"function": "getLibrarySectionDetails", "library": library.id, "status": status.status, "section": section.id})
+        .done(function(librarySectionDetails) {
           $.each(librarySectionDetails, function(librarySectionDetailID, librarySectionDetail) {
             addLibrarySectionDetail(library, status, section, librarySectionDetail);
           });
-          $('#libraries-loading').on('animationiteration', function() {
-            $(this).removeClass('fa-spin');
-          });
+          syncStopStatus('#libraries-loading');
+        })
+        .fail(function(jqxhr, textStatus, errorThrow) {
+          console.log(`getLibrarySectionDetails failed: ${jqxhr.status} (${jqxhr.statusText}), ${textStatus}, ${errorThrown}`);
+          syncErrorStatus('#libraries-loading');
         });
-      $(this).removeClass('fa-chevron-down').addClass('fa-chevron-up');
     } else if ($(`#library-${library.id}-${status.status}-section-${section.id}-details`).is(':hidden')) {
-      $(`#library-${library.id}-${status.status}-section-${section.id}-details`).show();
-      $(this).removeClass('fa-chevron-down').addClass('fa-chevron-up');
+      sectionOpen(`#library-${library.id}-${status.status}-section-${section.id}-details`);
+      sectionOpenStatus($(this).children('.section-details-collapse-icon'));
     } else if ($(`#library-${library.id}-${status.status}-section-${section.id}-details`).is(':visible')) {
-      $(`#library-${library.id}-${status.status}-section-${section.id}-details`).hide();
-      $(this).removeClass('fa-chevron-up').addClass('fa-chevron-down');
+      sectionClose(`#library-${library.id}-${status.status}-section-${section.id}-details`);
+      sectionCloseStatus($(this).children('.section-details-collapse-icon'));
     }
   });
 
-  $(document).on('click', 'span.refresh-library', function() {
+  $(document).on('click', '.refresh-library', function() {
     var library = $(this).data('library');
-    $('#libraries-loading').addClass('fa-spin');
-    $.get('query.php', {"function": "getLibraryDetails", "library": library.id})
-      .done(function(data) {
-        var libraryDetails = $.parseJSON(data);
+    syncStartStatus('#libraries-loading');
+    $.getJSON('query.php', {"function": "getLibraryDetails", "library": library.id})
+      .done(function(libraryDetails) {
         var visibleSections = [];
         $(`.library-${library.id}-section.card:visible`).each(function() {
           var visibleSection = [];
@@ -153,31 +187,41 @@ $(document).ready(function() {
         });
         $.each(visibleSections, function(visibleSectionID, visibleSection) {
           if ($(`#library-${visibleSection.library.id}-${visibleSection.status.status}-sections`).length) {
-            $(`#library-${visibleSection.library.id}-${visibleSection.status.status}-sections`).show();
-            $.get('query.php', {"function": "getLibrarySections", "library": visibleSection.library.id, "status": visibleSection.status.status})
-              .done(function(data) {
-                var librarySections = $.parseJSON(data);
+            sectionOpen(`#library-${visibleSection.library.id}-${visibleSection.status.status}-sections`);
+            sectionOpenStatus($(`#library-${visibleSection.library.id}-${visibleSection.status.status}-summary`).children('.sections-collapse-icon'));
+            $.getJSON('query.php', {"function": "getLibrarySections", "library": visibleSection.library.id, "status": visibleSection.status.status})
+              .done(function(librarySections) {
                 $.each(librarySections, function(librarySectionID, librarySection) {
                   addLibrarySection(visibleSection.library, visibleSection.status, librarySection);
                 });
                 $.each(visibleSection.details, function(visibleSectionDetailID, visibleSectionDetail) {
                   if ($(`#library-${visibleSection.library.id}-${visibleSection.status.status}-section-${visibleSectionDetail.section.id}`).length) {
-                    addLibrarySectionStatus(visibleSection.library, visibleSection.status, visibleSectionDetail.section);
-                    $.get('query.php', {"function": "getLibrarySectionDetails", "library": visibleSection.library.id, "status": visibleSection.status.status, "section": visibleSectionDetail.section.id})
-                      .done(function(data) {
-                        var librarySectionDetails = $.parseJSON(data);
+                    addLibrarySectionDetails(visibleSection.library, visibleSection.status, visibleSectionDetail.section);
+                    sectionOpenStatus($(`#library-${visibleSection.library.id}-${visibleSection.status.status}-section-${visibleSectionDetail.section.id}-summary`).children('.section-details-collapse-icon'));
+                    $.getJSON('query.php', {"function": "getLibrarySectionDetails", "library": visibleSection.library.id, "status": visibleSection.status.status, "section": visibleSectionDetail.section.id})
+                      .done(function(librarySectionDetails) {
                         $.each(librarySectionDetails, function(librarySectionDetailID, librarySectionDetail) {
                           addLibrarySectionDetail(visibleSection.library, visibleSection.status, visibleSectionDetail.section, librarySectionDetail);
                         });
-                      });
+                      })
+                     .fail(function(jqxhr, textStatus, errorThrow) {
+                       console.log(`getLibrarySectionDetails failed: ${jqxhr.status} (${jqxhr.statusText}), ${textStatus}, ${errorThrown}`);
+                       syncErrorStatus('#libraries-loading');
+                     });
                   }
                 });
+              })
+              .fail(function(jqxhr, textStatus, errorThrow) {
+                console.log(`getLibrarySections failed: ${jqxhr.status} (${jqxhr.statusText}), ${textStatus}, ${errorThrown}`);
+                syncErrorStatus('#libraries-loading');
               });
           }
         });
-        $('#libraries-loading').on('animationiteration', function() {
-          $(this).removeClass('fa-spin');
-        });
+        syncStopStatus('#libraries-loading');
+      })
+      .fail(function(jqxhr, textStatus, errorThrow) {
+        console.log(`getLibraryDetails failed: ${jqxhr.status} (${jqxhr.statusText}), ${textStatus}, ${errorThrown}`);
+        syncErrorStatus('#libraries-loading');
       });
   });
 });
