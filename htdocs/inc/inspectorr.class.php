bartmoorman/Docker-Inspectorr

commit 92b9c654bdd90a760d99fedb4e08c027317ed3a7

Apps: programmatic control

htdocs/inc/inspectorr.class.php
@@ -101,6 +101,22 @@ CREATE TABLE IF NOT EXISTS `events` (
   `message` BLOB,
   `remote_addr` INTEGER
 );
+CREATE TABLE IF NOT EXISTS `apps` (
+  `app_id` INTEGER PRIMARY KEY AUTOINCREMENT,
+  `name` TEXT NOT NULL,
+  `token` TEXT NOT NULL UNIQUE,
+  `begin` INTEGER,
+  `end` INTEGER,
+  `disabled` INTEGER NOT NULL DEFAULT 0
+);
+CREATE TABLE IF NOT EXISTS `calls` (
+  `call_id` INTEGER PRIMARY KEY AUTOINCREMENT,
+  `date` INTEGER DEFAULT (STRFTIME('%s', 'now')),
+  `app_id` INTEGER NOT NULL,
+  `action` TEXT,
+  `message` BLOB,
+  `remote_addr` INTEGER
+);
 EOQ;
     if ($this->dbConn->exec($query)) {
       return true;
@@ -109,14 +125,14 @@ EOQ;
   }
 
   public function isConfigured() {
-    if ($this->getCount('users')) {
+    if ($this->getObjectCount('users')) {
       return true;
     }
     return false;
   }
 
   public function isValidSession() {
-    if (array_key_exists('authenticated', $_SESSION) && $this->isValidUser('user_id', $_SESSION['user_id'])) {
+    if (array_key_exists('authenticated', $_SESSION) && $this->isValidObject('user_id', $_SESSION['user_id'])) {
       return true;
     }
     return false;
@@ -149,12 +165,22 @@ EOQ;
     return false;
   }
 
-  public function isValidUser($type, $value) {
+  public function isValidObject($type, $value) {
     $type = $this->dbConn->escapeString($type);
     $value = $this->dbConn->escapeString($value);
+    switch ($type) {
+      case 'username':
+      case 'user_id':
+        $table = 'users';
+        break;
+      case 'token':
+      case 'app_id':
+        $table = 'apps';
+        break;
+    }
     $query = <<<EOQ
 SELECT COUNT(*)
-FROM `users`
+FROM `{$table}`
 WHERE `{$type}` = '{$value}'
 AND NOT `disabled`;
 EOQ;
@@ -232,6 +258,29 @@ EOQ;
     return false;
   }
 
+  public function createApp($name, $token = null, $begin = null, $end = null) {
+    $token = !$token ? bin2hex(random_bytes(8)) : $this->dbConn->escapeString($token);
+    $query = <<<EOQ
+SELECT COUNT(*)
+FROM `apps`
+WHERE `token` = '{$token}';
+EOQ;
+    if (!$this->dbConn->querySingle($query)) {
+      $name = $this->dbConn->escapeString($name);
+      $begin = $this->dbConn->escapeString($begin);
+      $end = $this->dbConn->escapeString($end);
+      $query = <<<EOQ
+INSERT
+INTO `apps` (`name`, `token`, `begin`, `end`)
+VALUES ('{$name}', '{$token}', STRFTIME('%s','{$begin}',) STRFTIME('%s','{$end}'));
+EOQ;
+      if ($this->dbConn->exec($query)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
   public function updateUser($user_id, $username, $password = null, $first_name, $last_name = null, $role) {
     $user_id = $this->dbConn->escapeString($user_id);
     $username = $this->dbConn->escapeString($username);
@@ -269,31 +318,75 @@ EOQ;
     return false;
   }
 
-  public function modifyUser($action, $user_id) {
-    $user_id = $this->dbConn->escapeString($user_id);
+  public function updateApp($app_id, $name, $token, $begin, $end) {
+    $app_id = $this->dbConn->escapeString($app_id);
+    $token = $this->dbConn->escapeString($token);
+    $query = <<<EOQ
+SELECT COUNT(*)
+FROM `apps`
+WHERE `app_id` != '{$app_id}'
+AND `token` = '{$token}';
+EOQ;
+    if (!$this->dbConn->querySingle($query)) {
+      $name = $this->dbConn->escapeString($name);
+      $begin = $this->dbConn->escapeString($begin);
+      $end = $this->dbConn->escapeString($end);
+      $query = <<<EOQ
+UPDATE `apps`
+SET
+  `name` = '{$name}',
+  `token` = '{$token}',
+  `begin` = STRFTIME('%s', '{$begin}'),
+  `end` = STRFTIME('%s', '{$end}')
+WHERE `app_id` = '{$app_id}';
+EOQ;
+      if ($this->dbConn->exec($query)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public function modifyObject($action, $type, $value, $extra_type = null, $extra_value = null) {
+    $type = $this->dbConn->escapeString($type);
+    $value = $this->dbConn->escapeString($value);
+    $extra_type = $this->dbConn->escapeString($extra_type);
+    $extra_value = $this->dbConn->escapeString($extra_value);
+    switch ($type) {
+      case 'username':
+      case 'user_id':
+        $table = 'users';
+        $extra_table = 'events';
+        break;
+      case 'token':
+      case 'app_id':
+        $table = 'apps';
+        $extra_table = 'calls';
+        break;
+    }
     switch ($action) {
       case 'enable':
         $query = <<<EOQ
-UPDATE `users`
+UPDATE `{$table}`
 SET `disabled` = '0'
-WHERE `user_id` = '{$user_id}';
+WHERE `{$type}` = '{$value}';
 EOQ;
         break;
       case 'disable':
         $query = <<<EOQ
 UPDATE `users`
 SET `disabled` = '1'
-WHERE `user_id` = '{$user_id}';
+WHERE `{$type}` = '{$value}';
 EOQ;
         break;
       case 'delete':
         $query = <<<EOQ
 DELETE
-FROM `users`
-WHERE `user_id` = '{$user_id}';
+FROM `{$table}`
+WHERE `{$type}` = '{$value}';
 DELETE
-FROM `events`
-WHERE `user_id` = '{$user_id}';
+FROM `{$extra_table}`
+WHERE `{$type}` = '{$value}';
 EOQ;
         break;
     }
@@ -303,36 +396,58 @@ EOQ;
     return false;
   }
 
-  public function getUsers() {
-    $query = <<<EOQ
+  public function getObjects($type) {
+    switch ($type) {
+      case 'users':
+        $query = <<<EOQ
 SELECT `user_id`, `username`, `first_name`, `last_name`, `role`, `disabled`
 FROM `users`
-ORDER BY `last_name`, `first_name`
+ORDER BY `last_name`, `first_name`;
 EOQ;
-    if ($users = $this->dbConn->query($query)) {
+        break;
+      case 'apps':
+        $query = <<<EOQ
+SELECT `app_id`, `name`, `token`, `begin`, `end`, `disabled`
+FROM `apps`
+ORDER BY `name`;
+EOQ;
+        break;
+    }
+    if ($objects = $this->dbConn->query($query)) {
       $output = [];
-      while ($user = $users->fetchArray(SQLITE3_ASSOC)) {
-        $output[] = $user;
+      while ($object = $users->fetchArray(SQLITE3_ASSOC)) {
+        $output[] = $object;
       }
       return $output;
     }
     return false;
   }
 
-  public function getUserDetails($user_id) {
-    $user_id = $this->dbConn->escapeString($user_id);
-    $query = <<<EOQ
+  public function getObjectDetails($type, $value) {
+    $value = $this->dbConn->escapeString($value);
+    switch ($type) {
+      case 'user':
+        $query = <<<EOQ
 SELECT `user_id`, `username`, `first_name`, `last_name`, `role`, `disabled`
 FROM `users`
-WHERE `user_id` = '{$user_id}';
+WHERE `user_id` = '{$value}';
 EOQ;
-    if ($user = $this->dbConn->querySingle($query, true)) {
-      return $user;
+        break;
+      case 'app':
+        $query = <<<EOQ
+SELECT `app_id`, `name`, `token`, STRFTIME('%Y-%m-%dT%H:%M', `begin`, 'unixepoch') AS `begin`, STRFTIME('%Y-%m-%dT%H:%M', `end`, 'unixepoch') AS `end`, `disabled`
+FROM `apps`
+WHERE `app_id` = '{$value}';
+EOQ;
+        break;
+    }
+    if ($object = $this->dbConn->querySingle($query, true)) {
+      return $object;
     }
     return false;
   }
 
-  public function getCount($type) {
+  public function getObjectCount($type) {
     $type = $this->dbConn->escapeString($type);
     $query = <<<EOQ
 SELECT COUNT(*)

htdocs/apps.php
@@ -5,7 +5,7 @@ $inspectorr = new Inspectorr(true, true, true, false);
 <!DOCTYPE html>
 <html lang='en'>
   <head>
-    <title>Inspectorr - Users</title>
+    <title>Inspectorr - Apps</title>
     <meta charset='utf-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
     <link rel='stylesheet' href='//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css' integrity='sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB' crossorigin='anonymous'>
@@ -21,23 +21,24 @@ include_once('header.php');
         <thead>
           <tr>
             <th><button type='button' class='btn btn-sm btn-outline-success id-add'>Add</button></th>
-            <th>User ID</th>
-            <th>Username</th>
-            <th>User Name</th>
-            <th>Role</th>
+            <th>App ID</th>
+            <th>App Name</th>
+            <th>Begin</th>
+            <th>End</th>
           </tr>
         </thead>
         <tbody>
 <?php
-foreach ($inspectorr->getObjects('users') as $user) {
-  $user_name = !empty($user['last_name']) ? sprintf('%2$s, %1$s', $user['first_name'], $user['last_name']) : $user['first_name'];
-  $tableClass = $user['disabled'] ? 'text-warning' : 'table-default';
+foreach ($inspectorr->getObjects('apps') as $app) {
+  $begin = !empty($app['begin']) ? date('m/d/Y, h:i A', $app['begin']) : '&infin;';
+  $end = !empty($app['end']) ? date('m/d/Y, h:i A', $app['end']) : '&infin;';
+  $tableClass = $app['disabled'] ? 'text-warning' : 'table-default';
   echo "          <tr class='{$tableClass}'>" . PHP_EOL;
-  echo "            <td><button type='button' class='btn btn-sm btn-outline-info id-details' data-user_id='{$user['user_id']}'>Details</button></td>" . PHP_EOL;
-  echo "            <td>{$user['user_id']}</td>" . PHP_EOL;
-  echo "            <td>{$user['username']}</td>" . PHP_EOL;
-  echo "            <td>{$user_name}</td>" . PHP_EOL;
-  echo "            <td>{$user['role']}</td>" . PHP_EOL;
+  echo "            <td><button type='button' class='btn btn-sm btn-outline-info id-details' data-app_id='{$app['app_id']}'>Details</button></td>" . PHP_EOL;
+  echo "            <td>{$app['app_id']}</td>" . PHP_EOL;
+  echo "            <td>{$app['name']}</td>" . PHP_EOL;
+  echo "            <td>{$begin}</td>" . PHP_EOL;
+  echo "            <td>{$end}</td>" . PHP_EOL;
   echo "          </tr>" . PHP_EOL;
 }
 ?>
@@ -54,31 +55,22 @@ foreach ($inspectorr->getObjects('users') as $user) {
             <div class='modal-body'>
               <div class='form-row'>
                 <div class='form-group col'>
-                  <label>Username <sup class='text-danger' data-toggle='tooltip' title='Required'>*</sup></label>
-                  <input class='form-control' id='username' type='text' name='username' pattern='[A-za-z0-9]+' required>
+                  <label>App Name <sup class='text-danger' data-toggle='tooltip' title='Required'>*</sup></label>
+                  <input class='form-control' id='name' type='text' name='name' required>
                 </div>
                 <div class='form-group col'>
-                  <label>Password <sup class='text-danger id-required' data-toggle='tooltip' title='Required'>*</sup></label>
-                  <input class='form-control id-password' id='password' type='password' name='password' minlength='6' required>
-                </div>
-              </div>
-              <div class='form-row'>
-                <div class='form-group col'>
-                  <label>First Name <sup class='text-danger' data-toggle='tooltip' title='Required'>*</sup></label>
-                  <input class='form-control' id='first_name' type='text' name='first_name' required>
-                </div>
-                <div class='form-group col'>
-                  <label>Last Name</label>
-                  <input class='form-control' id='last_name' type='text' name='last_name'>
+                  <label>Access Token <sup class='text-danger id-required' data-toggle='tooltip' title='Required'>*</sup></label>
+                  <input class='form-control id-token' id='token' type='text' name='token' minlength='16' maxlength='16' pattern='[A-Za-z0-9]{16}' required>
                 </div>
               </div>
               <div class='form-row'>
                 <div class='form-group col'>
-                  <label>Role <sup class='text-danger' data-toggle='tooltip' title='Required'>*</sup></label>
-                  <select class='form-control' id='role' name='role' required>
-                    <option value='user'>user</option>
-                    <option value='admin'>admin</option>
-                  </select>
+                  <label>Begin</label>
+                  <input class='form-control' id='begin' type='datetime-local' name='begin'>
+                </div>
+                <div class='form-group col'>
+                  <label>End</label>
+                  <input class='form-control' id='end' type='datetime-local' name='end'>
                 </div>
               </div>
             </div>
@@ -100,33 +92,33 @@ foreach ($inspectorr->getObjects('users') as $user) {
         $('[data-toggle="tooltip"]').tooltip();
 
         $('button.id-add').click(function() {
-          $('h5.modal-title').text('Add User');
-          $('form').removeData('user_id').data('func', 'createUser').trigger('reset');
-          $('sup.id-required').removeClass('d-none');
-          $('input.id-password').prop('required', true);
-          $('button.id-modify').addClass('d-none').removeData('user_id');
+          $('h5.modal-title').text('Add App');
+          $('form').removeData('app_id').data('func', 'createApp').trigger('reset');
+          $('sup.id-required').addClass('d-none');
+          $('input.id-token').prop('required', false).attr('placeholder', 'Will be generated if empty');
+          $('button.id-modify').addClass('d-none').removeData('app_id');
           $('button.id-submit').removeClass('btn-info').addClass('btn-success').text('Add');
           $('div.id-modal').modal('toggle');
         });
 
         $('button.id-details').click(function() {
-          $('h5.modal-title').text('User Details');
-          $('form').removeData('user_id').data('func', 'updateUser').trigger('reset');
-          $('sup.id-required').addClass('d-none');
-          $('input.id-password').prop('required', false);
-          $('button.id-modify').removeClass('d-none').removeData('user_id');
+          $('h5.modal-title').text('App Details');
+          $('form').removeData('app_id').data('func', 'updateApp').trigger('reset');
+          $('sup.id-required').removeClass('d-none');
+          $('input.id-token').removeAttr('placeholder').prop('required', true);
+          $('button.id-modify').removeClass('d-none').removeData('app_id');
           $('button.id-submit').removeClass('btn-success').addClass('btn-info').text('Save');
-          $.get('src/action.php', {"func": "getObjectDetails", "type": "user", "value": $(this).data('user_id')})
+          $.get('src/action.php', {"func": "getObjectDetails", "type": "app", "value": $(this).data('app_id')})
             .done(function(data) {
               if (data.success) {
-                user = data.data;
-                $('form').data('user_id', user.user_id);
-                $('#username').val(user.username);
-                $('#first_name').val(user.first_name);
-                $('#last_name').val(user.last_name);
-                $('#role').val(user.role);
-                $('button.id-modify.id-volatile').data('action', user.disabled ? 'enable' : 'disable').text(user.disabled ? 'Enable' : 'Disable');
-                $('button.id-modify').data('user_id', user.user_id);
+                app = data.data;
+                $('form').data('app_id', app.app_id);
+                $('#name').val(app.name);
+                $('#token').val(app.token);
+                $('#begin').val(app.begin);
+                $('#end').val(app.end);
+                $('button.id-modify.id-volatile').data('action', app.disabled ? 'enable' : 'disable').text(app.disabled ? 'Enable' : 'Disable');
+                $('button.id-modify').data('app_id', app.app_id);
                 $('div.id-modal').modal('toggle');
               }
             })
@@ -136,8 +128,8 @@ foreach ($inspectorr->getObjects('users') as $user) {
         });
 
         $('button.id-modify').click(function() {
-          if (confirm(`Want to ${$(this).data('action').toUpperCase()} user ${$(this).data('user_id')}?`)) {
-            $.get('src/action.php', {"func": "modifyObject", "action": $(this).data('action'), "type": "user_id", "value": $(this).data('user_id')})
+          if (confirm(`Want to ${$(this).data('action').toUpperCase()} app ${$(this).data('app_id')}?`)) {
+            $.get('src/action.php', {"func": "modifyObject", "action": $(this).data('action'), "type": "app_id", "value": $(this).data('app_id')})
               .done(function(data) {
                 if (data.success) {
                   location.reload();
@@ -151,7 +143,7 @@ foreach ($inspectorr->getObjects('users') as $user) {
 
         $('form').submit(function(e) {
           e.preventDefault();
-          $.post('src/action.php', {"func": $(this).data('func'), "user_id": $(this).data('user_id'), "username": $('#username').val(), "password": $('#password').val(), "first_name": $('#first_name').val(), "last_name": $('#last_name').val(), "role": $('#role').val()})
+          $.post('src/action.php', {"func": $(this).data('func'), "app_id": $(this).data('app_id'), "name": $('#name').val(), "token": $('#token').val(), "begin": $('#begin').val(), "end": $('#end').val()})
             .done(function(data) {
               if (data.success) {
                 location.reload();

htdocs/events.php
@@ -49,7 +49,7 @@ foreach ($inspectorr->getEvents($currentPage) as $event) {
     <nav>
       <ul class='pagination justify-content-center'>
 <?php
-$pages = ceil($inspectorr->getCount('events') / $inspectorr->pageLimit);
+$pages = ceil($inspectorr->getObjectCount('events') / $inspectorr->pageLimit);
 $group = ceil($currentPage / 5);
 $previousPage = $currentPage - 1;
 $nextPage = $currentPage + 1;

htdocs/header.php
@@ -5,6 +5,7 @@ if ($inspectorr instanceof Inspectorr) {
   echo "      <button class='btn btn-sm btn-outline-success mr-auto id-nav' data-href='{$homeLoc}'>Home</button>" . PHP_EOL;
   if ($inspectorr->isAdmin()) {
     echo "      <button class='btn btn-sm btn-outline-info mr-2 id-nav' data-href='users.php'>Users</button>" . PHP_EOL;
+    echo "      <button class='btn btn-sm btn-outline-info mr-2 id-nav' data-href='apps.php'>Apps</button>" . PHP_EOL;
     echo "      <button class='btn btn-sm btn-outline-info mr-2 id-nav' data-href='events.php'>Events</button>" . PHP_EOL;
   }
   echo "      <button class='btn btn-sm btn-outline-danger id-nav' data-href='logout.php' title='Log out'><span class='fa fa-power-off'></span></button>" . PHP_EOL;

htdocs/src/action.php
@@ -33,6 +33,24 @@ switch ($_REQUEST['func']) {
       $output['message'] = 'Unauthorized';
     }
     break;
+  case 'createApp':
+    if ($dashboard->isValidSession() && $dashboard->isAdmin()) {
+      if (!empty($_REQUEST['name'])) {
+        $token = isset($_REQUEST['token']) ? $_REQUEST['token'] : null;
+        $begin = !empty($_REQUEST['begin']) ? $_REQUEST['begin'] : null;
+        $end = !empty($_REQUEST['end']) ? $_REQUEST['end'] : null;
+        $output['success'] = $dashboard->createApp($_REQUEST['name'], $token, $begin, $end);
+      } else {
+        header('HTTP/1.1 400 Bad Request');
+        $output['success'] = false;
+        $output['message'] = 'No name supplied';
+      }
+    } else {
+      header('HTTP/1.1 403 Forbidden');
+      $output['success'] = false;
+      $output['message'] = 'Unauthorized';
+    }
+    break;
   case 'updateUser':
     if ($inspectorr->isValidSession() && $inspectorr->isAdmin()) {
       if (!empty($_REQUEST['user_id']) && !empty($_REQUEST['username']) && !empty($_REQUEST['first_name']) && !empty($_REQUEST['role'])) {
@@ -51,12 +69,13 @@ switch ($_REQUEST['func']) {
       $output['message'] = 'Unauthorized';
     }
     break;
-  case 'modifyUser':
-    if ($inspectorr->isValidSession() && $inspectorr->isAdmin()) {
-      if (!empty($_REQUEST['action']) && !empty($_REQUEST['user_id'])) {
-        $output['success'] = $inspectorr->modifyUser($_REQUEST['action'], $_REQUEST['user_id']);
-        $log['action'] = $_REQUEST['action'];
-        $log['user_id'] = $_REQUEST['user_id'];
+  case 'updateApp':
+    if ($dashboard->isValidSession() && $dashboard->isAdmin()) {
+      if (!empty($_REQUEST['app_id']) && !empty($_REQUEST['name']) && !empty($_REQUEST['token'])) {
+        $begin = !empty($_REQUEST['begin']) ? $_REQUEST['begin'] : null;
+        $end = !empty($_REQUEST['end']) ? $_REQUEST['end'] : null;
+        $output['success'] = $dashboard->updateApp($_REQUEST['app_id'], $_REQUEST['name'], $_REQUEST['token'], $begin, $end);
+        $log['app_id'] = $_REQUEST['app_id'];
       } else {
         header('HTTP/1.1 400 Bad Request');
         $output['success'] = false;
@@ -68,20 +87,39 @@ switch ($_REQUEST['func']) {
       $output['message'] = 'Unauthorized';
     }
     break;
-  case 'getUserDetails':
+  case 'modifyObject':
     if ($inspectorr->isValidSession() && $inspectorr->isAdmin()) {
-      if (!empty($_REQUEST['user_id'])) {
-        if ($output['data'] = $inspectorr->getUserDetails($_REQUEST['user_id'])) {
+      if (!empty($_REQUEST['action']) && !empty($_REQUEST['type']) && !empty($_REQUEST['value'])) {
+        $output['success'] = $inspectorr->modifyObject($_REQUEST['action'], $_REQUEST['type'], $_REQUEST['value']);
+        $log['action'] = $_REQUEST['action'];
+        $log['type'] = $_REQUEST['type'];
+        $log['value'] = $_REQUEST['value'];
+      } else {
+        header('HTTP/1.1 400 Bad Request');
+        $output['success'] = false;
+        $output['message'] = 'Missing arguments';
+      }
+    } else {
+      header('HTTP/1.1 403 Forbidden');
+      $output['success'] = false;
+      $output['message'] = 'Unauthorized';
+    }
+    break;
+  case 'getObjectDetails':
+    if ($inspectorr->isValidSession() && $inspectorr->isAdmin()) {
+      if (!empty($_REQUEST['type']) && !empty($_REQUEST['value'])) {
+        if ($output['data'] = $inspectorr->getObjectDetails($_REQUEST['type'], $_REQUEST['value'])) {
           $output['success'] = true;
           $putEvent = false;
         } else {
           $output['success'] = false;
-          $log['user_id'] = $_REQUEST['user_id'];
+          $log['type'] = $_REQUEST['type'];
+          $log['value'] = $_REQUEST['value'];
         }
       } else {
         header('HTTP/1.1 400 Bad Request');
         $output['success'] = false;
-        $output['message'] = 'No user id supplied';
+        $output['message'] = 'Missing arguments';
       }
     } else {
       header('HTTP/1.1 403 Forbidden');

commit 03828650167f8e17bcfd8967ebc68ce77f09c951

Bug caused by 92b9c654bdd90a760d99fedb4e08c027317ed3a7

htdocs/inc/inspectorr.class.php
@@ -425,7 +425,7 @@ EOQ;
     }
     if ($objects = $this->dbConn->query($query)) {
       $output = [];
-      while ($object = $users->fetchArray(SQLITE3_ASSOC)) {
+      while ($object = $objects->fetchArray(SQLITE3_ASSOC)) {
         $output[] = $object;
       }
       return $output;