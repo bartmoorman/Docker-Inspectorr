fc781fabbdc2d6f5da58db70b622af46633a0e65
Bart Moorman <github@id10t.us>
2019-04-17 00:02:03 -0600

    General: Make managing css and js versions across the app a bit easier

--- htdocs/index.php
+++ htdocs/index.php
@@ -8,14 +8,10 @@ $inspectorr = new Inspectorr(true, true, false, false);
     <title>Inspectorr - Index</title>
     <meta charset='utf-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
-    <link rel='stylesheet' href='//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css' integrity='sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB' crossorigin='anonymous'>
-    <link rel='stylesheet' href='//maxcdn.bootstrapcdn.com/bootswatch/4.1.1/darkly/bootstrap.min.css' integrity='sha384-ae362vOLHy2F1EfJtpMbNW0i9pNM1TP2l5O4VGYYiLJKsaejqVWibbP6BSf0UU5i' crossorigin='anonymous'>
-    <link rel='stylesheet' href='//use.fontawesome.com/releases/v5.1.0/css/all.css' integrity='sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt' crossorigin='anonymous'>
+<?php require_once('include.css'); ?>
   </head>
   <body>
-<?php
-include_once('header.php');
-?>
+<?php require_once('header.php'); ?>
     <div class='container'>
       <div class='card'>
         <div class='card-header border-bottom-0'>
@@ -105,9 +101,7 @@ foreach ($inspectorr->statuses as $tab => $statuses) {
         </div>
       </div>
     </div>
-    <script src='//code.jquery.com/jquery-3.3.1.min.js' integrity='sha384-tsQFqpEReu7ZLhBV2VZlAu7zcOV+rXbYlF2cqB8txI/8aZajjp4Bqd+V6D5IgvKT' crossorigin='anonymous'></script>
-    <script src='//cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js' integrity='sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49' crossorigin='anonymous'></script>
-    <script src='//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js' integrity='sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T' crossorigin='anonymous'></script>
+<?php require_once('include.js'); ?>
     <script>
       $(document).ready(function() {
         $('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
